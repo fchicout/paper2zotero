6ad92de9806acb7520d1e97e8ba2cfc7efe13c6f
Fábio Chicout <fchicout@gmail.com>
2025-12-16 12:30:00 -0300

    Create ZoteroAPIClient skeleton in infra/zotero_api.py

---
+++ src/arxiv2zotero/infra/zotero_api.py
@@ -0,0 +1,26 @@
+import requests
+import re
+from typing import Optional
+
+from arxiv2zotero.core.interfaces import ZoteroGateway
+from arxiv2zotero.core.models import ArxivPaper
+
+class ZoteroAPIClient(ZoteroGateway):
+    API_VERSION = '3'
+    BASE_URL = 'https://api.zotero.org'
+
+    def __init__(self, api_key: str, group_id: str):
+        self.api_key = api_key
+        self.group_id = group_id
+        self.headers = {
+            'Zotero-API-Version': self.API_VERSION,
+            'Zotero-API-Key': self.api_key
+        }
+
+    def get_collection_id_by_name(self, name: str) -> Optional[str]:
+        # Placeholder for implementation
+        pass
+
+    def create_item(self, paper: ArxivPaper, collection_id: str) -> bool:
+        # Placeholder for implementation
+        pass
